7946bf252488a3dc0088d70d27c12f5a109939b6
Yuchuan <yyc478@gmail.com>
2017-11-20 03:32:55 +0800

    Add files for assignment 2

---
+++ Assignments/Assignment2/training_mpi.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#define GAME_START 0
+#define ROUND_START 1
+#define KICK_PHASE 2
+#define GAME_OVER 3
+
+struct location {
+  int x;
+  int y;
+}
+struct {
+
+}
+
+int main(int argc, char *argv[]) {
+
+
+
+
+}

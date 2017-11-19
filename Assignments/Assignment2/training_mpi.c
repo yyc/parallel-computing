d938525f1a07d1b6b236d13ca3eae595315a351c
Yuchuan <yyc478@gmail.com>
2017-11-20 06:25:07 +0800

    game structure in place

--- Assignments/Assignment2/training_mpi.c
+++ Assignments/Assignment2/training_mpi.c
@@ -7,6 +7,11 @@
 #define KICK_PHASE 2
 #define GAME_OVER 3
 
+#define DEFAULT_TAG 0
+
+#define FIELD_WIDTH 64
+#define FIELD_LENGTH 128
+
 /* Message format: 3-int array
    Field 1: Type, either GAME_START, ROUND_START, KICK_PHASE or GAME_OVER
    Depending on the type, the remaining fields:
@@ -15,25 +20,64 @@
  */
 
 struct playerInfo {
-  int posX;
-  int posY;
-  int rank;
+  int         posX;
+  int         posY;
+  int         rank;
+  int        *messageBuffer;
+  MPI_Request request;
+};
+
+// does an Isend to the person with the player's messageBuffer
+void IsendPlayer(struct playerInfo *player) {
+  MPI_Isend(player->messageBuffer, 3, MPI_INT, player->rank,
+            DEFAULT_TAG, MPI_COMM_WORLD, &(player->request));
 }
 
 // Master and field program. Always uses non-blocking sending and blocking
 // receiving
 void master(int numtasks) {
-  int i = 0;
-
+  int  i = 0;
+  int  numplayers = numtasks - 1;
   int  rounds = 0;
   int *buffer = (int *)malloc((sizeof(int) * 3));
+  int  ballX, ballY;
 
   printf("Initiating Master Field Program..\n");
 
-  // Initialize every player, send them their info
-  for (i = 1; i < numtasks; i++) {}
+  struct playerInfo players[numplayers];
 
-  while (rounds++ < 2) {}
+  // Initialize every player, send them their info
+  for (i = 0; i < numplayers; i++) {
+    // since the field has rank 0, and players are from 1-11
+    players[i].rank             = i + 1;
+    players[i].posX             = rand() % FIELD_WIDTH;
+    players[i].posY             = rand() % FIELD_LENGTH;
+    players[i].messageBuffer    = (int *)malloc((sizeof(int) * 3));
+    players[i].messageBuffer[0] = GAME_START;
+    players[i].messageBuffer[1] = players[i].posX;
+    players[i].messageBuffer[2] = players[i].posY;
+    IsendPlayer(&players[i]);
+  }
+
+  ballX = rand() % FIELD_WIDTH;
+  ballY = rand() % FIELD_LENGTH;
+
+  while (++rounds <= 2) {
+    printf("Master: =====ROUND %i START=====", rounds);
+
+    for (i = 0; i < numplayers; i++) {
+      players[i].messageBuffer[0] = ROUND_START;
+      players[i].messageBuffer[1] = ballX;
+      players[i].messageBuffer[2] = ballY;
+      IsendPlayer(&players[i]);
+    }
+  }
+
+  // Tell all players the game is up
+  for (i = 0; i < numplayers; i++) {
+    players[i].messageBuffer[0] = GAME_OVER;
+    IsendPlayer(&players[i]);
+  }
 }
 
 // Player program. Always using blocking sends and receives
@@ -44,7 +88,7 @@ void player(int rank) {
   int *buffer = (int *)malloc((sizeof(int) * 3));
 
   do {
-    MPI_RECV(buffer, 3, MPI_INT, 0, 0, MPI_COMM_WORLD, &stat);
+    MPI_Recv(buffer, 3, MPI_INT, 0, DEFAULT_TAG, MPI_COMM_WORLD, &stat);
     type = buffer[0];
 
     if (type == GAME_OVER) {
@@ -52,14 +96,16 @@ void player(int rank) {
     } else if (type == GAME_START) {
       posX = buffer[1];
       posY = buffer[2];
-      printf("Player %i Reporting In! I'm at %i, %i\n", rank, posX, posY);
+      printf("Player %i Reporting In! I'm at % i, % i \n", rank, posX, posY);
     } else if (type == ROUND_START) {
       ballX = buffer[1];
       ballY = buffer[2];
+      printf("New Round, ball at %i %i\n", ballX, ballY);
     } else if (type == KICK_PHASE) {}
     ballX = 0;
     ballY = 0;
   } while (type != GAME_OVER);
+  printf("Player %i Ending..\n", rank);
 }
 
 int main(int argc, char *argv[])
